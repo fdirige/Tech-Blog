2ea0b15ba8c2e996f824bffcc60d89fff33f2ae3
fdirige <118780727+fdirige@users.noreply.github.com>
2023-04-15 13:47:23 -0700

    Added public, db, and models

---
+++ public/js/dashboard.js
@@ -0,0 +1,47 @@
+var existingBlogs = document.querySelector("#existingblogs")
+var createNew = document.querySelector("#createNew")
+var newPost = document.querySelector("#newPost")
+var newBlog = document.querySelector("#newBlog")
+
+function hideCreateNew() {
+    createNew.hidden = true;
+}
+
+hideCreateNew();
+
+newPost.addEventListener("submit", event => {
+    event.preventDefault()
+    console.log('click')
+    existingBlogs.hidden = true;
+    newPost.hidden = true;
+    createNew.hidden = false;
+});
+
+newBlog.addEventListener("submit", event => {
+    var title = document.querySelector("#title").value;
+    var content = document.querySelector("#content").value
+    event.preventDefault()
+    console.log('Selected')
+    if (!title || !content) {
+        alert('Please enter a title and content')
+        return;
+    }
+    const blogObj = {
+        title: title,
+        content: content,
+    }
+    fetch("/api/blogs", {
+        method: "POST",
+        body: JSON.stringify(blogObj),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).then (res => {
+        if(res.ok){
+            createNew.setAttribute("hidden", "false")
+            location.reload()
+        } else {
+            alert("Error")
+        }
+    })
+})
