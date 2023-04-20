401b5f1f83dec887183d712de35532aa8f26f576
fdirige <118780727+fdirige@users.noreply.github.com>
2023-04-19 21:42:56 -0700

    Deleted Node_modules

--- server.js
+++ server.js
@@ -13,17 +13,7 @@ const { User, Blog, Comment } = require("./models");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-const sess = {
-    secret: process.env.DB_SESSION_SECRET,
-    cookie: {
-        maxAge: 0.5 * 60 * 60 * 1000 
-    },
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-        db: sequelize
-    })
-};
+
 
 app.use(express.static('public'));
 
